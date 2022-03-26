68b4b6f4ba28e04bb7828e04b60ef40e74498646
azirulislam <azirulislam27@gmail.com>
2022-03-26 12:23:18 +0600

    all added in ui

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 const Product = (props) => {
-    const {name, img, price} = props.Product;
+    const {name, img, price} = props.product;
     return (
         <div>
-           <img src={img} alt="" />
+        <img src={img} alt="" />
+          <h5>{name}</h5> 
+          <p>{price}</p>
+        
         </div>
     );
 };

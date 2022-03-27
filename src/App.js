9da1e56b31969805544386c6590011b891d13716
azirulislam <azirulislam27@gmail.com>
2022-03-27 21:31:42 +0600

    another commit adde

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Product from './components/Product/Product';
 
 function App() {
     const [products, setProduct] = useState([]);
+    const [cart, setCart] = useState([]);
 
     useEffect( () => {
       fetch('product.json')
@@ -11,19 +12,36 @@ function App() {
       .then(data => setProduct(data))
     },[]);
 
+    const showResult = (product) =>{
+      const addCart = [...cart, product];
+      setCart(addCart);
+    };
+
       return (
-    <div className="App">
-      <h2 className='header'>Welcome to my laptop Shop</h2>
-      <div className="product-container">
+          <div>
+            <h2 className='headline'>Welcome to my laptop Shop</h2>
+        <div className="product-container">
+        <div className='main-content'>
         {
           products.map(product => <Product 
             key={product.id}
             product={product}
+            showResult={showResult}
             ></Product>)
         }
       </div>
+      <div className='select-item'>
+        <h3>Your Product</h3>
+         <h3>{cart.length}</h3>
+        <button>CHOOSE 1 FOR ME</button>
+        <br/><br/>
+        <button>CHOOSE AGAIN</button>
+      </div>
     </div>
+    </div>
+   
   );
 }
 
 export default App;
+
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import './Product.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
 
 const Product = (props) => {
     const {name, img, price} = props.product;
     return (
         <div className='container'>
          <img src={img} alt="" />
-         <h5>{name}</h5> 
-          <p>{price}</p>
-
-        
-        </div>
+         <h5>Model: {name}</h5> 
+          <p>Price: {price}</p>
+        <button  onClick={() => props.showResult(props.product)} className='cart-btn'><p>Add to cart</p> 
+        <FontAwesomeIcon icon={faCartPlus} ></FontAwesomeIcon>
+         </button>
+         </div>
     );
+    
 };
 
 export default Product;

734ba0279f4c03ca1874ea09db1752acb1174cce
azirulislam <azirulislam27@gmail.com>
2022-03-27 22:56:13 +0600

    all commit added

--- src/App.js
+++ src/App.js
@@ -20,6 +20,13 @@ function App() {
       return (
           <div>
             <h2 className='headline'>Welcome to my laptop Shop</h2>
+            <div className='card'>
+            <h3>How react works</h3>
+            <p>React is a unidirectional Javascript library.it works with virtual DOM.React send data from parent to child.The JSX gets converted to react.creatElement function calls.The parent component send data to child via props.</p>
+
+            <h3>props vs state</h3>
+            <p>State use in usually parent component to store data.Props is use as a parameter in child component.State stored data use in parent component and then send data in props.Props data use in child component then it render in UI.State is a function own local scope but props are a parameter.</p>
+         </div>
         <div className="product-container">
         <div className='main-content'>
         {
@@ -32,7 +39,13 @@ function App() {
       </div>
       <div className='select-item'>
         <h3>Your Product</h3>
-         <h3>{cart.length}</h3>
+         <div>
+           {
+             cart.map(item =>(
+               <h5>{item.name}</h5>
+             ))
+           }
+         </div>
         <button>CHOOSE 1 FOR ME</button>
         <br/><br/>
         <button>CHOOSE AGAIN</button>
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,17 +3,19 @@ import './Product.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
 
-const Product = (props) => {
-    const {name, img, price} = props.product;
+
+const Product = ({product, showResult}) => {
+    const {name, img, price} = product;
     return (
         <div className='container'>
          <img src={img} alt="" />
          <h5>Model: {name}</h5> 
           <p>Price: {price}</p>
-        <button  onClick={() => props.showResult(props.product)} className='cart-btn'><p>Add to cart</p> 
+        <button  onClick={() => showResult(product)} className='cart-btn'><p>Add to cart</p> 
         <FontAwesomeIcon icon={faCartPlus} ></FontAwesomeIcon>
          </button>
          </div>
+      
     );
     
 };
